d85052713902da33fd4a11c4c91410cb262e9f87
Pedro Rocha <pedrovasrocha@edu.unifor.br>
2024-11-22 00:28:56 -0300

    Adicinado metodos e correção do construtor

--- src/Sessao.java
+++ src/Sessao.java
@@ -1,28 +1,33 @@
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Sessao {
     private int idSessao;
-    private dataSessaoHora datetime;
+    private Date dataHoraSessao;
     private Filme filme;
     private Sala sala;
     private Funcionario funcionario;
     private String status;
 
-    public Sessao(String cpf, String nome, String email, int matricula, date horarioTrabalho, int idSessao, dataSessaoHora datetime, Filme filme, Sala sala, Funcionario funcionario, String status) {
-        super(cpf, nome, email, matricula, horarioTrabalho);
+    public Sessao(int idSessao, Date dataHoraSessao, Filme filme, Sala sala, Funcionario funcionario, String status) {
         this.idSessao = idSessao;
-        this.datetime = datetime;
+        this.dataHoraSessao = dataHoraSessao;
         this.filme = filme;
         this.sala = sala;
         this.funcionario = funcionario;
         this.status = status;
     }
-
     public int getIdSessao() {
         return idSessao;
     }
     public void setIdSessao(int idSessao) {
         this.idSessao = idSessao;
+    }
+    public Date getDataHoraSessao() {
+        return dataHoraSessao;
+    }
+    public void setDataHoraSessao(Date dataHoraSessao) {
+        this.dataHoraSessao = dataHoraSessao;
     }
     public Filme getFilme() {
         return filme;
@@ -30,29 +35,23 @@ public class Sessao {
     public void setFilme(Filme filme) {
         this.filme = filme;
     }
-    public dataSessaoHora getDatetime() {
-        return datetime;
-    }
-    public void setDatetime(dataSessaoHora datetime) {
-        this.datetime = datetime;
-    }
     public Sala getSala() {
         return sala;
     }
     public void setSala(Sala sala) {
         this.sala = sala;
     }
-    public Funcionario getFuncionario() {
-        return funcionario;
-    }
-    public void setFuncionario(Funcionario funcionario) {
-        this.funcionario = funcionario;
-    }
     public String getStatus() {
         return status;
     }
     public void setStatus(String status) {
         this.status = status;
+    }
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
     }
     public boolean cadastrar(Sessao sessao){
         return false;
